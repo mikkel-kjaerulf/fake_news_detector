46c5694d89eb9009152e64c1d632eca79e6870ba
JulianBarraganG <xjulianbarragan@gmail.com>
2023-03-21 15:02:28 +0100

    fixed cleaning

--- cleaning_functions.py
+++ cleaning_functions.py
@@ -19,21 +19,22 @@ import time
 mail_pattern = re.compile(r"(?P<Mails>[\w\.-]+@[\w\.-]+\.[\w]+)")
 url_pattern = re.compile(r"(?P<URL>(?:(?:https?|ftp):[/][/])?[\w/\-?=%.]+\.[\w/\-&?=%.]+)")
 date_pattern = re.compile(r"([\d]{1,2}[\/|\-][\d]{1,2}(?:[\/|\-][\d]{2,4})?|[\d]{2,4}[\/|\-][\d]{1,2}[\/|\-][\d]{1,2}|(?:january|february|march|april|may|june|july|august|september|october|november|december)[\s][\d]{1,2}[a-z][a-z](?:\s[\d]{2,4})|[\d][\d]\w?\w?\sof\s(?:january|february|march|april|may|june|july|august|september|october|november|december)(?:\s[\d]{2,4})?|(?:january|february|march|april|may|june|july|august|september|october|november|december)\s\d\d?\w?\w?,?(?:\s\d{2,4})?)")
-num_pattern = re.compile(r"\w*\d+\w*") 
-punct_remove = re.compile(r"[^\w\s]+")
+num_pattern = re.compile(r"[a-zA-Z]*\d+[a-zA-Z]*") 
+punct_remove = re.compile(r"[^a-zA-Z\s]+")
 white_space = re.compile(r"[\s]+|\n+")
+
 
 
 """
 Cleans the text from mail/url/date/whitespace/punctuation
 """
 def clean_single_string(st):
-    s1 = clean(st.lower(), lang='en')
+    s1 = st.lower()
     s2 = mail_pattern.sub("mail", s1)
     s3 = url_pattern.sub("url", s2)
     s4 = date_pattern.sub("date", s3)
     s5 = num_pattern.sub("num", s4)
-    s6 = clean(s5, no_punct=True)
+    s6 = punct_remove.sub(" ", s5)
     s7 = white_space.sub(" ", s6)
     return s7
 
